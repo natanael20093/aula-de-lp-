430518933d202ee98c2bdcad52ae144b0d0a0c78
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-15 16:36:25 -0300

    fest: tipagem e operacoes matematicas

---
+++ calculos.py
@@ -0,0 +1,20 @@
+import os 
+os.system("cls")
+
+primeiro_numero:int
+
+segundo_numero:int
+
+primeiro_numero = float (input("digite o primeiro numero:"))
+
+segundo_numero = float (input("digite o segundo numero:"))
+
+soma =primeiro_numero + segundo_numero
+subtracao= primeiro_numero - segundo_numero
+multiplicacao= primeiro_numero * segundo_numero
+divisao= primeiro_numero / segundo_numero
+
+print(f"soma: ", soma)
+print(F"SUBTRAÇÃO: " , subtracao)
+print(f"multiplicacao", multiplicacao)
+print(f"divisaõ: ", divisao)
---
+++ tipagem.py
@@ -0,0 +1,9 @@
+ # tipagem dinamica.
+
+import os
+os.system("cls")
+nome = input("digete seu nome:")
+idade = input("digite sua idade:")
+  
+print(f"nome: {nome}")
+print(f"idade: {idade}")
